bc26965e6ca3e8d4510ef142747ec840b52d3dd2
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-10-18 23:18:07 +0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const app = require("./app.js");
 
+const port = 3002;
+
 const server = http.createServer(app);
 server.listen(port, () => {
     console.log(`connect ${port}`)
