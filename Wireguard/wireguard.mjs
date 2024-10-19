7bc235983aea7d3f6c9ecd21d09178dd6a2bf9ed
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-10-19 19:18:51 +0600

    update

--- Wireguard/wireguard.mjs
+++ Wireguard/wireguard.mjs
@@ -2,7 +2,6 @@ import path from 'path'
 import { WgConfig, createPeerPairs } from "wireguard-tools"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { execSync } from 'child_process';
 import fs from 'fs';
 import { execSync, exec } from 'child_process';
 
