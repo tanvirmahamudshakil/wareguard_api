88970aa5c3b2ec3976a5beda8907582d48af95af
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-10-19 08:54:58 +0600

    update

--- app.mjs
+++ app.mjs
@@ -12,7 +12,11 @@ const app = express();
 
 app.get("/header", async (req, res) => {
     var data = await config1.generateKeys()
-    res.status(200).send(data.publicKey);
+    var config = await config1.writeToFile()
+
+    // bring up
+    await config1.up()
+    res.status(200).send(config);
 });
 
 
