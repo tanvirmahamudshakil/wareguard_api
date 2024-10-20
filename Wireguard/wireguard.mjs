c87e41ea157c1d2fc356408d4124813b57509b56
tanvirmahamudshakil <shakilhassan887@gmail.com>
2024-10-21 01:47:07 +0600

    update

--- Wireguard/wireguard.mjs
+++ Wireguard/wireguard.mjs
@@ -388,11 +388,11 @@ function parseWireGuardOutput(output) {
             }
         }
 
-        // If there's no handshake at all, mark peer as inactive
-        if (!handshakeMatch && lastPeer) {
-            lastPeer.latestHandshake = "Never connected";
-            lastPeer.inactive = true; // Mark as inactive if there's no handshake found
-        }
+        // // If there's no handshake at all, mark peer as inactive
+        // if (!handshakeMatch && lastPeer) {
+        //     lastPeer.latestHandshake = "Never connected";
+        //     lastPeer.inactive = true; // Mark as inactive if there's no handshake found
+        // }
 
 
     });
